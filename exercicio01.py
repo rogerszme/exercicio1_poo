88fd493d3f3254e504bc0230f0d4e03e2be54b9d
Rogério Gomes <rogerszme@hotmail.com>
2024-09-25 21:32:54 -0300

    aula 1 exercício no laboratório
    import os
    os.system('cls')
    # limpa o terminal 
    # 1- O que é python ?
    # 2-Qual o programa IDE executa o arquivo .py
    # 3- Qual linguagem de programação  Python substituiu ?
    message1 = "É uma linguagem de programação de alto nível"
    message2 = "Qualquer (IDE) que tenha o compilador python instalado"
    message3 = 'Linguagem de programação ABC'
    
    print('O que é "python"?\nRESPOSTA:',message1)
    print('##################################################################')
    print('Qual o programa (IDE) executa o arquivo ".PY"?\nRESPOSTA:',message2)
    print('##################################################################')
    print('Qual linguagem de programação Python substituiu ?\nRESPOSTA:',message3)

---
+++ exercicio01.py
@@ -0,0 +1,15 @@
+import os
+os.system('cls')
+# limpa o terminal 
+# 1- O que é python ?
+# 2-Qual o programa IDE executa o arquivo .py
+# 3- Qual linguagem de programação  Python substituiu ?
+message1 = "É uma linguagem de programação de alto nível"
+message2 = "Qualquer (IDE) que tenha o compilador python instalado"
+message3 = 'Linguagem de programação ABC'
+
+print('O que é "python"?\nRESPOSTA:',message1)
+print('##################################################################')
+print('Qual o programa (IDE) executa o arquivo ".PY"?\nRESPOSTA:',message2)
+print('##################################################################')
+print('Qual linguagem de programação Python substituiu ?\nRESPOSTA:',message3)
